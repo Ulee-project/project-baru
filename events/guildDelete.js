a0992f6fd2d2f7e57ec32ed7e355927ef5643438
Mewew <48173337+Bartholomeouw@users.noreply.github.com>
2020-04-25 10:54:29 +0700

    Update guildDelete.js

--- events/guildDelete.js
+++ events/guildDelete.js
@@ -1,4 +1,4 @@
 module.exports = (client, guild) => {
   console.log(`Left: ${guild.name}`)
-  client.channels.get('696911061399502950').send({embed: {color: 0xd30750, description: `Left: **${guild.name}**`}})
+  client.channels.get('703452961329643571').send({embed: {color: 0xd30750, description: `Left: **${guild.name}**`}})
 }
