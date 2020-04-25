7a08f929308e0badac99c250c36a7f62fd0fbe02
Mewew <48173337+Bartholomeouw@users.noreply.github.com>
2020-04-25 10:53:17 +0700

    Update guildCreate.js

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -1,4 +1,4 @@
 module.exports = (client, guild) => {
   console.log(`Join: ${guild.name}`)
-  client.channels.get('696911061399502950').send({embed: {color: 0x4db2b6, description: `Invited to: **${guild.name}**\nServer Member: **${guild.memberCount}**\nServer Owner: **${guild.owner.user.username}**`}})
+  client.channels.get('703452961329643571').send({embed: {color: 0x4db2b6, description: `Invited to: **${guild.name}**\nServer Member: **${guild.memberCount}**\nServer Owner: **${guild.owner.user.username}**`}})
 }
