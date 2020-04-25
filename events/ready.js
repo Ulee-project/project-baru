fd647db21c5e254950d45c801caa9c61730456fd
Mewew <48173337+Bartholomeouw@users.noreply.github.com>
2020-04-25 10:46:54 +0700

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,5 +1,5 @@
 module.exports = client => {
   console.log('Connected.');
   console.log('Aku cinta ray,  tpi boong')
-  client.user.setActivity(`lala!help Partners ${client.guilds.size} Teitoku~`, {type: "WATCHING"});
+  client.user.setActivity(`lala!help, Music Bot.`, {type: "WATCHING"});
 }
